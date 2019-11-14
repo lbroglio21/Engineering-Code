fd2a494a934fdf1aa37f8740626f00eaacd6b057
ndoering22 <ndoering@bsmschool.org>
2019-11-14 09:41:40 -0600

    Comepleted autoBrake

--- mod36/autoBrake.py
+++ mod36/autoBrake.py
@@ -6,7 +6,28 @@ sensor_pin = 16
 RPL.pinMode(sensor_pin,RPL.INPUT)
 
 running=True
-
+braking = False
+m1Speed = 1400 
+m2Speed = 1600
 while running:
-  
+    reading=RPL.digitalRead(sensor_pin)
+    
+    if reading ==0:
+        braking = True
+    if braking == True and m1Speed!=1500 and m2Speed!=1500:
+        print("If statement fired")
+        m1Speed+=10
+        m2Speed-=10
+    if m1Speed == 1500 and m2Speed == 1500:
+        m1Speed =00
+        m2Speed =00
+        running=False
 
+    RPL.servoWrite(0,m1Speed)
+    RPL.servoWrite(1,m2Speed)
+        
+    PTW.state['d1'] = RPL.digitalRead(sensor_pin)
+    PTW.state['d2'] = RPL.servoRead(0)
+    PTW.state['d3'] = RPL.servoRead(1)
+    PTW.post()
+   
---
+++ mod36/stopMove.py
@@ -0,0 +1,5 @@
+import RoboPiLib as RPL
+import setup
+
+RPL.servoWrite(0,00)
+RPL.servoWrite(1,00)
