a7ce51f21456f3528559a903f1b5f98523d6953a
cparece22 <cparece22@bsmschool.org>
2019-11-15 09:26:03 -0600

    Wokred on evadeWall

--- mod36/evadeWall.py
+++ mod36/evadeWall.py
@@ -1,35 +1,45 @@
 import RoboPiLib as RPL
 import setup
 import post_to_web as PTW
+import time 
 
 sensor_pin = 16
 sensor_pin1 = 17
 RPL.pinMode(sensor_pin,RPL.INPUT)
 RPL.pinMode(sensor_pin1,RPL.INPUT)
+
 running=True
 wallSeen = False
 seenTwice = False
 m1Speed = 1400 
 m2Speed = 1600
+t=time.time()
+ts=time.time()
+tw=0
 
 while running:
+    t=time.time()
+    rt=int(t-ts)
     reading=RPL.digitalRead(sensor_pin)
     reading1=RPL.digitalRead(sensor_pin1)
     if reading ==0:
         wallSeen  = True
-
+    elif reading==1:
+        wallSeen = False
     if wallSeen == True and seenTwice==False:
-        m2Speed-=50
+        m2Speed-=150
         seenTwice=True
-    elif wallSeen == False and reading1==0:
-        m2Speed=1600
-        seenTwice=False
+        tw=rt 
+        print(tw)
+    if rt - tw == 5:  
+        m2Speed = 1600
+        seenTwice = False
 
     if m1Speed == 1500 and m2Speed == 1500:
         m1Speed =00
         m2Speed =00
         running=False
-
+    
     RPL.servoWrite(0,m1Speed)
     RPL.servoWrite(1,m2Speed)
         
